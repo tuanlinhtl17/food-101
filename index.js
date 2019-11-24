7ffb636aba8ed5e80ab7262fb469d7243c813410
tl17 <tuanlinhtl17@gmail.com>
2019-11-24 16:08:34 +0700

    doi sang cong 80

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function () {
-	console.log('server running on port 3000');
+app.listen(80, function () {
+	console.log('server running on port 80');
 })
 
 // Predict image
